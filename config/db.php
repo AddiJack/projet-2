1eeeb50141bfa6f6f826c8b5a9333f59f1a45882
AddiJack <44784763+AddiJack@users.noreply.github.com>
2019-05-10 12:07:58 +0200

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: addi
+ * Date: 2019-04-10
+ * Time: 13:24
+ */
+
+define('APP_DB_USER', 'root');
+define('APP_DB_PWD', 'root');
+define('APP_DB_HOST', 'localhost');
+define('APP_DB_NAME', 'strascook');
